7709f15aacfe106c560a526dcce03fe98e4a6b8a
KZhiganov <KZhiganov@zvezdamedia.ru>
2026-02-11 13:06:48 +0300

    Добавил график

--- src/entities/table/ui/row-table/RowTable.tsx
+++ src/entities/table/ui/row-table/RowTable.tsx
@@ -1,8 +1,9 @@
-import React, { FC } from 'react';
+import React, { FC, useState } from 'react';
 import { classNames } from '@/shared/libs/helpers';
 import styles from './RowTable.module.scss';
 import { TableData } from '@/shared/libs/types';
 import { ColumnTable } from '@/entities/table/ui/column-table/ColumnTable';
+import { HighchartsLine } from '@/widgets/chart/Chart';
 
 interface RowTableProps {
   className?: string;
@@ -10,13 +11,27 @@ interface RowTableProps {
 }
 
 export const RowTable: FC<RowTableProps> = ({ className, rowData }) => {
+  const [showChart, setShowChart] = useState(false);
   const quantityColumn = (rowData ? Object.keys(rowData) : []) as (keyof TableData)[];
 
+  const clickToRow = () => {
+    setShowChart((pre) => !pre);
+  };
+
   return (
-    <tr className={classNames(styles.RowTable, [className])}>
-      {quantityColumn.map((item) => {
-        return <ColumnTable today={item === 'today'} dataColumn={rowData[item]} key={item} />;
-      })}
-    </tr>
+    <>
+      <tr className={classNames(styles.RowTable, [className])} onClick={clickToRow}>
+        {quantityColumn.map((item) => {
+          return <ColumnTable today={item === 'today'} dataColumn={rowData[item]} key={item} />;
+        })}
+      </tr>
+      {showChart && (
+        <tr className={classNames(styles.RowTable, [className])}>
+          <td colSpan={4}>
+            <HighchartsLine />
+          </td>
+        </tr>
+      )}
+    </>
   );
 };
